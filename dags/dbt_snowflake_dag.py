4cfebef6dce7c29c15b1a47f1b76a2255b8d9762
derinj7 <derin.j@astronomer.io>
2025-06-10 12:10:06 +0530

    Updated the latest files

--- dags/dbt_snowflake_dag.py
+++ dags/dbt_snowflake_dag.py
@@ -1,31 +1,33 @@
-from datetime import datetime, timedelta
-from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
+from datetime import datetime
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, RenderConfig, LoadMode
 from cosmos.profiles import SnowflakeUserPasswordProfileMapping
 
 # Using Airflow connection with username/password authentication
 profile_config = ProfileConfig(
-    profile_name="snowflake_demo",
-    target_name="dev",
-    profile_mapping=SnowflakeUserPasswordProfileMapping(
-        conn_id="snowflake_default",  # Your Airflow connection ID
-        profile_args={
-            # Only specify fields that are NOT in your Airflow connection
-            # or fields you want to override
-            "schema": "ANALYTICS",
-            "threads": 1,
-        },
-    )
+   profile_name="snowflake_demo",
+   target_name="dev",
+   profile_mapping=SnowflakeUserPasswordProfileMapping(
+       conn_id="snowflake_default",
+       profile_args={
+           "schema": "ANALYTICS",
+           "threads": 4,
+       },
+   )
+)
+
+# Use the manifest.json generated in CI/CD
+render_config = RenderConfig(
+   load_method=LoadMode.DBT_MANIFEST,
+   manifest_path="/usr/local/airflow/dbt/snowflake_demo/target/manifest.json",
 )
 
 dbt_snowflake_dag = DbtDag(
-    project_config=ProjectConfig(
-        dbt_project_path="/usr/local/airflow/dbt/snowflake_demo",
-    ),
-    profile_config=profile_config,
-    execution_config=ExecutionConfig(
-        dbt_executable_path="/usr/local/airflow/dbt_venv/bin/dbt",
-    ),
-    schedule="@daily",
-    start_date=datetime(2024, 1, 1),
-    dag_id="dbt_snowflake_cosmos_dag",
+   project_config=ProjectConfig(
+       dbt_project_path="/usr/local/airflow/dbt/snowflake_demo",
+   ),
+   profile_config=profile_config,
+   render_config=render_config,
+   schedule="@daily",
+   start_date=datetime(2024, 1, 1),
+   dag_id="dbt_snowflake_cosmos_dag",
 )
